3c81d43db1cfc6c5df185eb3045fed3523e4fe71
basel_slieman <bass889ha@gmail.com>
2024-07-17 01:44:02 +0300

    test mongodb module

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const {query,validationResult,matchedData} = require('express-validator');
-
+const {MongoClient}=require('mongodb');
+const url = "mongodb+srv://unifiuser:unifi2024@unifidb.0y81c1u.mongodb.net/?retryWrites=true&w=majority&appName=unifidb";
+const client =new MongoClient(url);
+const main=async()=>{
+  await client.connect();
+  console.log("cinnected succefully");
+  const db=client.db("unifisolutions");
+  const collection =db.collection("mover");
+  const data=await collection.find().toArray();
+  console.log("data",data);
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mongodb', function(req, res, next) {
+  main();
+});
 
 router.get('/hello', query('person').notEmpty().escape(), (req, res) => {
   const result = validationResult(req);
